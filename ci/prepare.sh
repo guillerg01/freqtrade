66dd5cf854fe5c6c60ebdcb7133b7c3802cd2456
Albert Rolando Enriquez Estrada <albertr3264@gmail.com>
2026-03-20 18:33:43 -0400

    Update prepare.sh

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,4 +1,5 @@
 sudo mkdir -p ~/bot/main/user_data/strategies/
 sudo mkdir -p ~/bot/main/user_data/logs/
+sudo chown -R 1000:1000 ~/bot/main/user_data/
 cp -r ~/bot/strategies/ ~/bot/main/user_data/strategies/
 cp ~/bot/config.json ~/bot/main/user_data/
