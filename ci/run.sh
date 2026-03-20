477fe398cbc845c50b6c723443924094a6777635
Albert Rolando Enriquez Estrada <albertr3264@gmail.com>
2026-03-20 18:25:40 -0400

    updating docker config

--- ci/run.sh
+++ ci/run.sh
@@ -2,5 +2,4 @@
 set -euo pipefail
 BOT_ROOT="${BOT_ROOT:-$HOME/bot/main}"
 cd "$BOT_ROOT"
-sudo docker compose pull
-sudo docker compose up -d --remove-orphans
+sudo docker compose up -d --build --remove-orphans
